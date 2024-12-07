71ec6c73948caef63968c841f553d532cf01690c
HassanRajpoot <xhah450@gmail.com>
2024-12-07 22:53:55 +0500

    Session commit

--- routes/api.php
+++ routes/api.php
@@ -8,5 +8,9 @@ use App\Http\Controllers\CsrfCookieController;
 Route::get('/sanctum/csrf-cookie', [CsrfCookieController::class, 'show']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::resource('people', PersonController::class);
+    Route::get('/people', [PeopleController::class, 'index'])->name('api.people.index');
+    Route::post('/people', [PeopleController::class, 'store'])->name('api.people.store');
+    Route::get('/people/{id}', [PeopleController::class, 'show'])->name('api.people.show');
+    Route::put('/people/{id}', [PeopleController::class, 'update'])->name('api.people.update');
+    Route::delete('/people/{id}', [PeopleController::class, 'destroy'])->name('api.people.destroy');
 });
